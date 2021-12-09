eb8756b5caa3e338ce46bacf096fd78fc267bab2
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-09 14:50:48 -0800

    fix router

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ function Header() {
     return (<>
         <div className="container">
             <ul className="Header" >
-                <Link to="/"><li>HOME</li></Link>
+                <Link to="/home"><li>HOME</li></Link>
                 <Link to="/favorite"><li>Favorite</li></Link>
                 <Link to="/rated"><li>Rated</li></Link>
             </ul>
