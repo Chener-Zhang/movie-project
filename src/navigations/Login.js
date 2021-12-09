e556328949de4a55d7a171275784cf01f2503b5a
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-08 22:05:08 -0800

    update

--- src/navigations/Login.js
+++ src/navigations/Login.js
@@ -8,34 +8,49 @@ import { Redirect } from 'react-router-dom'
 import '../styles/LoginPage.css'
 
 
+
 export default function Login() {
 
     const [userName, setUserName] = useState('');
+    const [passWord, setPassWord] = useState('');
     const isLogged = useSelector(state => state.LogReducer);
     const dispatch = useDispatch()
 
 
-    
+    async function login() {
+        console.warn(userName,passWord)
+        const response  = await fetch()
+    }
+    const checkUserInfo = () => {
+        console.log('button click')
+
+        if (userName == "" || passWord == "") {
+            console.log('it empty')
+        }
+
+        // dispatch(USER_LOGIN(userName))
+    }
+
+
     const userLogged = () => {
         if (!isLogged.Boolean) {
             return (
                 <div className="loginStyle">
                     <h2>LOGIN</h2>
-                    <h3> Username:
-                        <input onChange={(e) => setUserName(e.target.value)} />
-                    </h3>
-                    <h3> Password:
-                        <input />
-                    </h3>
-                    <button onClick={() =>
-                        dispatch(USER_LOGIN(userName))
-                    }>LOGIN </button>
+
+                    <input onChange={(e) => setUserName(e.target.value)} placeholder="UserName" />
+                    <input onChange={(e) => setPassWord(e.target.value)} placeholder="Passwords" />
+
+                    <button onClick={checkUserInfo}>LOGIN </button>
                 </div>)
 
         } else {
             return (<Redirect from="/login" to="/" />)
         }
     }
+
+
+
     return (
         <>
             {userLogged()}
