66b356dae0667db395a7e3a6ff809ce157b696eb
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-09 20:38:57 -0800

    home setup

--- src/navigations/HOME.js
+++ src/navigations/HOME.js
@@ -1,10 +1,16 @@
-
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux'
+import axios from 'axios'
 
 function HOME() {
 
     const isLogged = useSelector(state => state.LogReducer);
-    console.log(isLogged);
+    const api_key = 'dd32c1edcdcaa2ef3be79570c191e5ea';
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        console.log('userEffect was trigger')
+    });
     return (<div>This is the home page</div>);
 }
 
