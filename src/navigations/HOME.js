1e29681dbe6ef022db1b172f123d81eeda4844bc
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-10 09:58:42 -0800

    setup the playing now api call

--- src/navigations/HOME.js
+++ src/navigations/HOME.js
@@ -4,12 +4,26 @@ import axios from 'axios'
 
 function HOME() {
 
+
     const isLogged = useSelector(state => state.LogReducer);
     const api_key = 'dd32c1edcdcaa2ef3be79570c191e5ea';
 
-    useEffect(() => {
+    useEffect(async () => {
         // Update the document title using the browser API
+        console.log(isLogged);
         console.log('userEffect was trigger')
+        const result = await axios.get("https://api.themoviedb.org/3/movie/now_playing?", {
+            params: {
+                api_key: api_key
+            }})
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.warn(error);
+            })
+            
+
     });
     return (<div>This is the home page</div>);
 }
