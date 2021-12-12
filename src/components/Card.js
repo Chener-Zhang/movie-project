97332f49a3a5070daaca5650f57f2b87d7ac5d36
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-12 11:55:29 -0800

    set the favor button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsHeartFill, BsHeart } from 'react-icons/bs';
 
 
-export class Card extends React.Component {
+function Card(props) {
 
-    render() {
-        const info = this.props.movieInfo;
-        return (<div>
-            <div>
-                <img src={`https://image.tmdb.org/t/p/w500/${info.backdrop_path}`} width="300px" height="300px" />
+    const info = props.movieInfo;
+    const [favor, setFavor] = useState(false);
 
-                <h2>{info.title}</h2>
-            </div>
-            <div>
-                <h3>{info.vote_average}</h3>
-                <BsHeart />
-                <BsHeartFill />
-            </div>
+    function checkFavor() {
+        if (favor) {
+            return <BsHeartFill onClick={() => {
+                setFavor(false)
+            }} />
+        } else {
+            return <BsHeart onClick={() => {
+                setFavor(true)
+            }} />
+        }
+    }
 
-        </div>)
-    };
+    return (<div>
+        <div>
+            <img src={`https://image.tmdb.org/t/p/w500/${info.backdrop_path}`} width="300px" height="300px" />
+
+            <h2>{info.title}</h2>
+        </div>
+        <div>
+            <h3>{info.vote_average}</h3>
+            {checkFavor()}
+        </div>
+
+    </div>)
+
 };
 
 export default Card;
