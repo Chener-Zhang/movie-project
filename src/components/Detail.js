b03df80d972aa2c47289ea12038621543080186a
chenerzhang8@gmail.com <chenerzhang8@gmail.com>
2021-12-13 13:39:49 -0800

    success rate the movie

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select'
 import { RATE } from '../actions/rateAction';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Detail(props) {
 
     const [data, setData] = useState(null);
     const [rate, setRate] = useState(null);
+    const [rateHistory, setRatehistory] = useState(null);
+    const isLogged = useSelector(state => state.LogReducer);
+
     const movieId = props.match.params.id;
     const api_key = 'dd32c1edcdcaa2ef3be79570c191e5ea';
     const dispatch = useDispatch();
@@ -30,24 +33,31 @@ function Detail(props) {
                 setData(res.data)
             })
             .catch(e => console.log(e))
-    }, []);
 
+    }, [rateHistory]);
+
+ 
     function onClickHandler() {
         console.log(rate)
         if (rate !== null) {
             dispatch(RATE(data.id, rate));
         }
     }
+
     function showDetail() {
         return (<>
             <img src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`} width="400px" height="707px" alt={data.id} />
+
             <h1>{data.original_title}</h1>
+
             <h4>Release data :
-                <h3>{data.release_date}</h3>
+                {data.release_date}
             </h4>
+
             <h4>Overview:
-                <h3>{data.overview}</h3>
+                {data.overview}
             </h4>
+
             <h4>Genres:
                 <ul>
                     {data.genres.map(e => {
@@ -55,12 +65,13 @@ function Detail(props) {
                     })}
                 </ul>
             </h4>
-            <h4>Average Rating:
-                <h3>{data.vote_average}</h3>
-            </h4>
-            {/* your rate here */}
 
-            <h4>Your Rating</h4>
+            <h4>Average Rating:
+                {data.vote_average}
+            </h4>
+
+            <h4>Number : {rateHistory}</h4>
+
             <Select
                 defaultValue={options[0]}
                 onChange={(e) => {
@@ -68,6 +79,7 @@ function Detail(props) {
                 }}
                 options={options}
             />
+
             <button onClick={() => {
                 onClickHandler()
             }}>Submit</button>
@@ -75,14 +87,16 @@ function Detail(props) {
             <h4>Production Companies:
                 <ul>
                     {data.production_companies.map(e => {
-                        return <li key={e.id}>
+                        return
+                        (<li key={e.id}>
                             {e.name}
                             <img src={`https://image.tmdb.org/t/p/w500/${e.logo_path}`} width="50px" height="30" alt={e.id} />
-                        </li>
+                        </li>)
                     })}
                 </ul>
             </h4>
-        </>);
+        </>
+        );
     }
     return (
         <div>
